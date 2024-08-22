7a4f60037b7feebccab58ca1ca86d9c815e9cf8d
JohniFirst <admin@outlook.com>
2024-08-22 22:50:32 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  <h1 class="test">HELLO WORLD</h1>
+</template>
+
+<style>
+.test {
+  font-size: 30px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'amfe-flexible'
+
+import '@/assets/css/reset.css'
+
+createApp(App).mount('#app')

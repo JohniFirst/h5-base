b73f128661c5288e5a66a7d460abfcda0b6babd8
JohniFirst <a15708295047@outlook.com>
2024-09-18 14:49:24 +0000

    feat: 增加vite预加载失败的钩子函数

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,8 @@ import router from './routes/routes'
 const app = createApp(App)
 
 app.use(router).mount('#app')
+
+window.addEventListener('vite:preloadError', () => {
+  // window.location.reload() // 例如，刷新页面
+  alert('预加载失败，请刷新页面')
+})

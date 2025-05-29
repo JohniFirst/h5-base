4eee456cf90951b5f1b28f85b535409d687fb9d3
JohniFirst <a15708295047@outlook.com>
2025-05-29 23:37:46 +0800

    feat: 组件的泛型

---
+++ src/components-test/child-type.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>这是子组件</h2>
+
+  <ul>
+    <li v-for="(item, index) in data.arrTest" :key="index">
+      <slot :item="item" :index="index">
+        <span>{{ JSON.stringify(item) }}</span>
+      </slot>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts" generic="T">
+export type ChildArr<T> = {
+  name: string
+  age: number
+  arrTest: T[]
+}
+
+defineProps<{
+  msg: string
+  data: ChildArr<T>
+}>()
+</script>
---
+++ src/components-test/use-component.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>这是使用组件</h2>
+
+  <ChildType msg="这是子组件" :data="data">
+    <template #default="{ item }">
+      <span @click="handleClick(item)">{{ item.name }} - {{ item.age }}</span>
+    </template>
+  </ChildType>
+</template>
+
+<script setup lang="ts">
+import ChildType from './child-type.vue'
+
+type Item = {
+  name: string
+  age: number
+}
+
+const data = {
+  name: '张三',
+  age: 18,
+  arrTest: [
+    { name: '李四', age: 20 },
+    { name: '王五', age: 22 },
+  ] as Item[],
+}
+
+const handleClick = (item: Item) => {
+  console.log(item)
+}
+</script>
+
+<style lang="less" scoped></style>
